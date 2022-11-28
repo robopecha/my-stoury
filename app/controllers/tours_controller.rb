bb1cfb0655fadb0b67d1bce05fd1db0491493415
Julia Kolb <julikolb@gmail.com>
2022-11-28 16:52:06 +0100

    controllers views

--- app/controllers/tours_controller.rb
+++ app/controllers/tours_controller.rb
@@ -1,4 +1,5 @@
 class ToursController < ApplicationController
+  before_action :set_tour, only: %i[show]
 
   def index
     @tours = Tour.all
@@ -8,5 +9,28 @@ class ToursController < ApplicationController
     @tour = Tour.find(params[:id])
     @site = site.new
   end
-  
+
+  def new
+    @tour = Tour.new
+  end
+
+  def create
+    @tour = Tour.new(tour_params)
+    @tour.user = current_user
+    if @tour.save
+      redirect_to tour_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def tour_params
+    params.require(:tour).permit(:name, :description, :img_url, :privacy)
+  end
+
+  def set_tour
+    @tour = Tour.find(params[:id])
+  end
 end
