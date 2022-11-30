acbfc6fad34082cd51dede6849bad30f218463ef
Lukas Jerusalem <lukasjerusalem@web.de>
2022-11-30 17:36:20 +0100

    seeds

--- app/models/site.rb
+++ app/models/site.rb
@@ -3,10 +3,8 @@ class Site < ApplicationRecord
   after_validation :geocode, if: :will_save_change_to_address?
   has_one_attached :photo
 
-  has_many :notes
+  has_many :notes, dependent: :destroy
   belongs_to :tour
-
-
+  
   validates :name, :address, presence: true
-
 end
