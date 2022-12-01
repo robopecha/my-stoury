40d35133c6e714b80e5337683ce3f868a1c6bff4
Laura Löschmann <44237401+LauraLoe@users.noreply.github.com>
2022-12-01 17:34:12 +0100

    first chat

--- app/controllers/chatrooms_controller.rb
+++ app/controllers/chatrooms_controller.rb
@@ -1,7 +1,24 @@
 class ChatroomsController < ApplicationController
 
+  def new
+    @chatroom = Chatroom.new
+  end
+
+  def create
+    @chatroom = Chatroom.new(chatroom_params)
+    @chatroom.save
+    @tour = Tour.find(params[:id])
+    redirect_to tour_path(@tour)
+  end
+
   def show
     @chatroom = Chatroom.find(params[:id])
     @message = Message.new
+  end
+
+  private
+
+  def chatroom_params
+    params.require(:chatroom).permit(:name)
   end
 end
--- app/models/tour.rb
+++ app/models/tour.rb
@@ -2,6 +2,7 @@ class Tour < ApplicationRecord
   has_many :sites, dependent: :destroy
   has_many :tour_users
   has_many :users, through: :tour_users
+  has_one :chatroom, dependent: :destroy
 
   belongs_to :user
   has_one_attached :photo
