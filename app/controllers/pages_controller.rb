9dc2342c3c55bf3b78feedad54c8b09e76419d00
Laura Löschmann <44237401+LauraLoe@users.noreply.github.com>
2022-12-04 17:31:07 +0100

    modal for add tour/user

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,10 +1,13 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [ :home ]
+  skip_before_action :authenticate_user!, only: :home
 
   def home
+    @tour = Tour.new
   end
 
   def dashboard
     @tours = current_user.all_tours
+    @tour = Tour.new
   end
+
 end
